40dd4d40d15c84256fc10e6cbfb9e63800acbd1c
loickcherimont <loickcherimont@gmail.com>
2023-10-20 07:57:26 +0200

    Duplicate each input to process each cases

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,19 +1,27 @@
 import { InputReadOnly } from "./InputReadOnly"
+import { Logo } from "./Logo"
+import logo from "../assets/img/twitter_logotest.png"
 
-{/* Row with logo and different logins for a specific application */}
-export function Row({ logo, codes }) {
-    console.log(codes)
-    return <div className="flex border border-black rounded m-3">
-        <div className="bg-yellow-400">{logo}</div>
-        <ul className="codes bg-orange-400 w-full">
-            {
-                
-                codes.map((input, index) => {
-                    return <li key={index} className="mt-2">
-                        <InputReadOnly value={input} type={"text"}/>
-                    </li>
-                })
-            }
+{/* Row with logo and different logins for a specific application */ }
+export function Row() {
+    return <div className="flex rounded mx-20 my-3 p-3 justify-evenly bg-white shadow-md items-center">
+        <Logo src={logo} />
+        <ul className="codes w-auto p-3">
+            {/* Actual [codes] is a sample */}
+
+            {/* codes={["www.loremipsum.com", "testlck", "test123"]} */}
+            <li className="mt-3">
+                <InputReadOnly value={"www.loremipsum.com"} type={"text"} />
+            </li>
+            <li className="mt-3">
+                <InputReadOnly value={"testlck"} type={"text"} />
+            </li>
+            {/* space-x-4 ??? */}
+            <li className="mt-3 flex space-x-4">
+                <InputReadOnly value={"testlck"} type={"password"} />
+                <button type="button" className="border border-slate-100 shadow-sm hover:bg-slate-100/50 transition duration-300 ease-in px-2 rounded">show/hide</button>
+            </li>
+
         </ul>
     </div>
 }
