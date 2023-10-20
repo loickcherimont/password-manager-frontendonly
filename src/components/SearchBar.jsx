65efc25535504d934ced2b3c005d4bebf493661b
loickcherimont <loickcherimont@gmail.com>
2023-10-20 07:57:09 +0200

    Remove building borders

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,13 +11,13 @@ export function SearchBar({placeholder, type}) {
         console.log(e.target.value)
     }
 
-    return <div className="border border-yellow-500 w-full mx-5">
+    return <div className="w-full mx-5">
         <input
             type={type}
             placeholder={placeholder}
             value={input}
             onChange={handleChange}
-            className="w-full p-3 border border-red-500 rounded"
+            className="w-full p-3 rounded"
         />
     </div>
 }
