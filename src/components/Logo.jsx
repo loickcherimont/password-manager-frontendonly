1a8fba968c02300fd766f2fec5fed98f0e2ab12f
loickcherimont <loickcherimont@gmail.com>
2023-10-20 07:57:52 +0200

    Create a Logo component for each target app

---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+// Logo for each target applications
+export function Logo({src}) {
+    return <div className="text-center flex justify-center items-center w-24 h-24">
+        <img src={src} alt="twitter test logo"/>
+    </div>
+}
