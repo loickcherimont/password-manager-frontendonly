30109070c2684c45cb37ea184ca8d8746f958aae
loickcherimont <loickcherimont@gmail.com>
2023-10-20 07:58:51 +0200

    For accessibility, change Brand color to white

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,3 +1,4 @@
+// For logo for the password manager application
 export function Brand({name}) {
-    return <h1 className="text-2xl text-center align-middle pt-7 font-bold h-24">{name}</h1>
+    return <h1 className="text-2xl text-center text-white align-middle pt-7 font-bold h-24">{name}</h1>
 }
