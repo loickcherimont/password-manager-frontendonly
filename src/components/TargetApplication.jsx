1651e2571766089fd2b021fb732d690029b280a3
loickcherimont <loickcherimont@gmail.com>
2023-10-24 07:32:47 +0200

    Create a TargetApplication comp. for different links

---
+++ src/components/TargetApplication.jsx
@@ -0,0 +1,5 @@
+export function TargetApplication({children}) {
+    return <div className="rounded mx-20 my-3 p-3 justify-evenly bg-white shadow-md items-center">
+        {children}
+    </div>
+}
