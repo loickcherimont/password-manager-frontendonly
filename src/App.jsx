c08b13c7799d9dfccec3c9b16683b21c20e80eca
loickcherimont <loickcherimont@gmail.com>
2023-10-24 07:33:34 +0200

    Allow the code transfer between form and all the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import { Brand } from "./components/Brand"
-import { Row } from "./components/Row"
 import { Form } from "./components/Form"
+import { useState } from "react"
+import { InputReadOnly } from "./components/InputReadOnly";
+import { Logo } from "./components/Logo";
+import logo from "./assets/img/twitter_logotest.png"
+import { TargetApplication } from "./components/TargetApplication";
 
 function App() {
+
+  const [codes, setCodes] = useState([
+    <TargetApplication>
+      <Logo src={logo} />
+
+      {/* To Do : Create a <link-login-password> component */}
+      <div className="mt-3">
+        <InputReadOnly value={"www.loremipsum.com"} type={"text"} />
+      </div>
+      <div className="mt-3">
+        <InputReadOnly value={"testlck"} type={"text"} />
+      </div>
+      <div className="mt-3 flex space-x-4">
+        <InputReadOnly value={"testlck"} type={"password"} />
+        <button type="button" className="border border-slate-100 shadow-sm hover:bg-slate-100/50 transition duration-300 ease-in px-2 rounded">show/hide</button>
+      </div>
+    </TargetApplication>
+  ]);
 
   return (
     // Tip: Use h-auto to fill all available space in the browser 
@@ -15,19 +37,28 @@ function App() {
       <main className="w-3/4 bg-slate-100 h-full">
         {/* Header */}
         <header className="h-24 flex justify-center items-center">
-          <Form/>
+          <Form codes={codes} setCodes={setCodes} />
         </header>
 
         {/* Center */}
         <div className="center">
-          {/* Row with logo and different logins for a specific application */}
-          <Row />
-          <Row />
-          <Row />
+
+          <ul>
+            {
+              codes.map((targetApplication, index) => {
+                return <li key={index} className="row mt-2">
+                  {targetApplication}
+                </li>
+              })
+            }
+          </ul>
+
+
+
         </div>
       </main>
 
-      
+
 
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
+import { InputReadOnly } from "./InputReadOnly";
+import { Logo } from "./Logo";
+import logo from "../assets/img/twitter_logotest.png"
 
 // Form to add new passwords
-export function Form() {
+export function Form({ codes, setCodes }) {
 
     // Informations for the attributes of inputs
     const fields = [
@@ -30,6 +33,26 @@ export function Form() {
         const link = formData.get("link");
         const login = formData.get("login");
         const password = formData.get("password");
+
+        const newCodes = [
+            <div className="rounded mx-20 my-3 p-3 justify-evenly bg-white shadow-md items-center">
+                <Logo src={logo} />
+
+                <div className="mt-3">
+                    <InputReadOnly value={link} type={"text"} />
+                </div>
+                <div className="mt-3">
+                    <InputReadOnly value={login} type={"text"} />
+                </div>
+                <div className="mt-3 flex space-x-4">
+                    <InputReadOnly value={password} type={"password"} />
+                    <button type="button" className="border border-slate-100 shadow-sm hover:bg-slate-100/50 transition duration-300 ease-in px-2 rounded">show/hide</button>
+                </div>
+            </div>
+        ];
+
+        // Update codes when data are submitted
+        setCodes([...codes, ...newCodes]);
     }
 
     return <div className="w-full mx-5 grid place-items-center">
