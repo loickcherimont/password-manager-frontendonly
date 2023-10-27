aae1cda5d55cbd00c2fc6398fa9595ff4728d8af
loickcherimont <loickcherimont@gmail.com>
2023-10-27 07:33:52 +0200

    Create an Aside instead Brand for logo

---
+++ src/components/Aside.jsx
@@ -0,0 +1,6 @@
+// About logo side banner
+export function Aside({title}) {
+    return <aside className="bg-teal-500 h-auto w-1/4">
+        <h1 className="text-2xl text-center text-white align-middle pt-7 font-bold h-24">{title}</h1>
+      </aside>
+}
--- src/components/Brand.jsx
+++
@@ -1,4 +0,0 @@
-// For logo for the password manager application
-export function Brand({name}) {
-    return <h1 className="text-2xl text-center text-white align-middle pt-7 font-bold h-24">{name}</h1>
-}
