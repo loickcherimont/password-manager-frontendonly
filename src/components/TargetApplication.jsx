b8a11e1c0d23c8f7c78b175c28425e1c51dfe2a1
loickcherimont <loickcherimont@gmail.com>
2023-10-30 07:47:34 +0100

    Add flex value to apply flex layout on inner TargetApplications

--- src/components/TargetApplication.jsx
+++ src/components/TargetApplication.jsx
@@ -1,5 +1,6 @@
+// Applications on UI with login/link/password data
 export function TargetApplication({children}) {
-    return <div className="rounded mx-20 my-3 p-3 justify-evenly bg-white shadow-md items-center">
+    return <div className="rounded mx-20 my-3 p-3 bg-white shadow-md flex justify-around items-center">
         {children}
     </div>
 }
