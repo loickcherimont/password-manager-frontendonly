ed9cc9bbaebfdb8f2e9d9e6f76adccf9c6a2a9b8
loickcherimont <loickcherimont@gmail.com>
2023-10-30 07:46:35 +0100

    Remove Aside for Logo component (main title, target apps only)

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,5 @@
 // Logo for each target applications
+// Possible fix : Replace logo for each target app by logo only for the main title of the Password App 
 export function Logo({src}) {
     return <div className="text-center flex justify-center items-center w-24 h-24">
         <img src={src} alt="twitter test logo"/>
