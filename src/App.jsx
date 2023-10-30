1fe909de2c4a9e3207fa93383da0ee77b22d69eb
loickcherimont <loickcherimont@gmail.com>
2023-10-30 07:46:01 +0100

    Store inputs in a new container ".inputs"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Aside } from "./components/Aside"
 import { Form } from "./components/Form"
 import { useState } from "react"
 import { InputReadOnly } from "./components/InputReadOnly";
@@ -6,33 +5,38 @@ import { Logo } from "./components/Logo";
 import logo from "./assets/img/twitter_logotest.png"
 import { TargetApplication } from "./components/TargetApplication";
 import { Password } from "./components/Password";
+import { ToggleForm } from "./components/ToggleForm";
 
 function App() {
 
   const [codes, setCodes] = useState([
     <TargetApplication>
       <Logo src={logo} />
-
-      {/* To Do : Create a <link-login-password> component */}
-      <div className="mt-3">
-        <InputReadOnly value={"www.loremipsum.com"} type={"text"} />
+      <div className="inputs">
+        {/* To Do : Create a <link-login-password> component */}
+        <div className="mt-3">
+          <InputReadOnly value={"www.loremipsum.com"} type={"text"} />
+        </div>
+        <div className="mt-3">
+          <InputReadOnly value={"testlck"} type={"text"} />
+        </div>
+        <Password value={"testlck"} />
       </div>
-      <div className="mt-3">
-        <InputReadOnly value={"testlck"} type={"text"} />
-      </div>
-      <Password value={"testlck"}/>
     </TargetApplication>
   ]);
 
   return (
     // Tip: Use "min-h-screen max-h-auto" to fill all available space in the browser 
     <div className="app flex min-h-screen max-h-auto">
-      <Aside title={"My Accesses 🔐"}/>
       {/* MAIN */}
-      <main className="w-3/4 bg-slate-100 min-h-screen max-h-auto">
+      <main className="w-full lg:w-3/4 bg-slate-100 min-h-screen max-h-auto">
         {/* Header */}
-        <header className="h-24 flex justify-center items-center">
-          <Form codes={codes} setCodes={setCodes} />
+        <header className="flex justify-between bg-gradient-to-r from-cyan-500 to-teal-300  lg:justify-center lg:items-center border border-black lg:h-24  lg:bg-transparent">
+          
+          <Form codes={codes} setCodes={setCodes} /> {/* On mobile, form are by default hidden to see ToggleForm and Logo */}
+          
+          <Logo src={logo} />
+          <ToggleForm />
         </header>
 
         {/* Center */}
