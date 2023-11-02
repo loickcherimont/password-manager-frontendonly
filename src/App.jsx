4b9d21b66d12af213d6771b48d0fc4c330cddf69
loickcherimont <loickcherimont@gmail.com>
2023-11-02 16:36:30 +0100

    Create a button to display mobile form

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import logo from "./assets/img/twitter_logotest.png"
 import { TargetApplication } from "./components/TargetApplication";
 import { Password } from "./components/Password";
 import { ToggleForm } from "./components/ToggleForm";
+import { MobileNavigationBtn } from "./components/MobileNavigationBtn";
 
 function App() {
 
+  // Storage for codes
   const [codes, setCodes] = useState([
     <TargetApplication>
       <Logo src={logo} />
       <div className="inputs">
-        {/* To Do : Create a <link-login-password> component */}
         <div className="mt-3">
           <InputReadOnly value={"www.loremipsum.com"} type={"text"} />
         </div>
@@ -25,18 +26,23 @@ function App() {
     </TargetApplication>
   ]);
 
+  const [userAskForm, setUserAskForm] = useState(false);
+
   return (
     // Tip: Use "min-h-screen max-h-auto" to fill all available space in the browser 
     <div className="app flex min-h-screen max-h-auto">
       {/* MAIN */}
-      <main className="w-full lg:w-3/4 bg-slate-100 min-h-screen max-h-auto">
+      <main className="w-full bg-slate-100 min-h-screen max-h-auto">
         {/* Header */}
-        <header className="flex justify-between bg-gradient-to-r from-cyan-500 to-teal-300  lg:justify-center lg:items-center border border-black lg:h-24  lg:bg-transparent">
-          
-          <Form codes={codes} setCodes={setCodes} /> {/* On mobile, form are by default hidden to see ToggleForm and Logo */}
-          
+        <header className="flex justify-between bg-gradient-to-r from-cyan-500 to-teal-300  lg:justify-center lg:items-center lg:h-24">
           <Logo src={logo} />
-          <ToggleForm />
+
+          <Form codes={codes} setCodes={setCodes} /> {/* On mobile, form are by default hidden to see ToggleForm and Logo */}
+
+          <ToggleForm userAskForm={userAskForm} codes={codes} setCodes={setCodes}/>
+
+          {/* Only for responsive */}
+          <MobileNavigationBtn setUserAskForm={setUserAskForm}/>
         </header>
 
         {/* Center */}
