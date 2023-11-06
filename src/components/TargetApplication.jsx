c9483e8c62506cc37fd34f79202c09647f721978
loickcherimont <loickcherimont@gmail.com>
2023-11-06 17:02:38 +0100

    Adapt to mobile, tablet screens

--- src/components/TargetApplication.jsx
+++ src/components/TargetApplication.jsx
@@ -1,6 +1,6 @@
 // Applications on UI with login/link/password data
 export function TargetApplication({children}) {
-    return <div className="rounded mx-20 my-3 p-3 bg-white shadow-md flex justify-around items-center">
+    return <div className="rounded mx-20 my-3 p-3 bg-white shadow-md flex justify-around items-center flex-wrap">
         {children}
     </div>
 }
