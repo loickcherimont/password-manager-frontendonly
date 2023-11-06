24b393a8eb2fb78987f77c6ee57aa100a188b5a5
loickcherimont <loickcherimont@gmail.com>
2023-11-06 17:03:39 +0100

    Move mobile button a little bit on the left

--- src/components/MobileNavigationBtn.jsx
+++ src/components/MobileNavigationBtn.jsx
@@ -4,7 +4,7 @@ export function MobileNavigationBtn({setUserAskForm}) {
 
     const handleClick = () => setUserAskForm(true);
 
-    return <button onClick={handleClick} type="button" className="lg:hidden">
+    return <button onClick={handleClick} type="button" className="lg:hidden mr-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
